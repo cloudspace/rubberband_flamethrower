8051d2feebe7ac62cf75644df315ed991c15da9c
Michael Orr <michael@cloudspace.com>
2013-04-01 17:58:35 -0400

    making it easier to update which word files are used for the random generation, cleaning up a little

--- inserter.rb
+++ inserter.rb
@@ -2,27 +2,35 @@ require 'httparty'
 require "active_support/core_ext"
 
 #combine several word files into an array of words
-contents = File.read('./words/american-words.35')
-words = contents.split(/\n/)
-contents = File.read('./words/american-words.20')
-words = words + contents.split(/\n/)
-contents = File.read('./words/american-words.10')
-words = words + contents.split(/\n/)
-puts "number of total base words: #{words.length}"
+words = []
+word_files = [
+  # "./words/american-words.95",
+  # "./words/american-words.80",
+  # "./words/american-words.70",
+  # "./words/american-words.60",
+  # "./words/american-words.55",
+  # "./words/american-words.50",
+  # "./words/american-words.40",
+  "./words/american-words.35",
+  "./words/american-words.20",
+  "./words/american-words.10"
+  ]
+word_files.each do |word_file|
+  contents = File.read(word_file)
+  words = words + contents.split(/\n/)
+end
 
+# set a starting id
 id = 2
 
 #start infinite loop
-while true do
-  
+while true do  
   #create a message from between 6 and 16 random words that maxes at 140 characters
   number_of_words = 6 + rand(10)
   sentence = (number_of_words.times.map{words.sample}.join(" ")+".")[0,140]
-  #puts "sample sentence id #{id}\n#{sentence}\nnumber of characters #{sentence.length}"
   
   # create a username that is only letters and numbers
   username = words.sample.gsub(/[^0-9a-z]/i, '')
-  #puts "by user: #{username}"
   
   #set a timestamp
   date = Time.now.strftime "%Y%m%dT%H:%M:%S"
@@ -31,15 +39,7 @@ while true do
   puts "twitter_message: #{twitter_message}"
   
   response = HTTParty.put("http://localhost:9200/twitter/tweet/#{id}", body: twitter_message) 
-  puts response.inspect
   puts response.body
-  #insert into elasticsearch with curl
-  # curl -XPUT localhost:9200/twitter/tweet/{id} -d '
-  # {
-  #   user : "{username}",
-  #   message : "{sentence}",
-  #   postDate : "{date}"
-  # }'
-
+  
   id = id + 1
 end

08dbad463e8cd420fec1a3dd8e86e1a903971458
Michael Orr <michael@cloudspace.com>
2013-04-03 10:30:55 -0400

    step one of converting to a gem - this is a non functioning gem commit

---
+++ lib/rubberband_flamethrower/data_generator.rb
@@ -0,0 +1,61 @@
+# This class is specially designed to generate random data to be inserted into Elastic Search
+# It creates a JSON object that approaxiamates data like you would fine in a tweet
+# with fields for a message (max 140 characters), username, and post_date.
+# The post_date format is parsable as a date object by default by Elastic Search
+class DataGenerator
+  attr_accessor :word_list
+  
+  # the WORD_FILES constant is an array of included word files
+  # which will be used to create the pool of random words used for data generation.
+  # You can uncomment or comment particular files to change the size of the pool of words.
+  # Please see the README file in the words folder for more information about the lists.
+  WORD_FILES = [
+    # "./words/american-words.95",
+    # "./words/american-words.80",
+    # "./words/american-words.70",
+    # "./words/american-words.60",
+    # "./words/american-words.55",
+    # "./words/american-words.50",
+    # "./words/american-words.40",
+    "./words/american-words.35",
+    "./words/american-words.20",
+    "./words/american-words.10"
+    ]
+
+  # Will initialize the word_list variable with an array of all the words contained in the WORD_FILES array   
+  def initialize
+    self.word_list = []
+    WORD_FILES.each do |word_file|
+      contents = File.read(word_file)
+      self.word_list = word_list + contents.split(/\n/)
+    end
+  end
+  
+  # create a message from between 6 and 16 random words that maxes at 140 characters and ends with a period
+  # @return [String]
+  def random_tweet
+    number_of_words = 6 + rand(10)
+    ((number_of_words.times.map{word_list.sample}.join(" "))[0,139])+"."
+  end
+  
+  # create a random value to be used as a username
+  # the return value is one random word, only letters and numbers allowed
+  # @return [String]
+  def random_username
+    word_list.sample.gsub(/[^0-9a-z]/i, '')
+  end
+  
+  # create an Elastic Search friendly timestamp for right now
+  # @return [String]
+  def current_timestamp
+    Time.now.strftime "%Y%m%dT%H:%M:%S"
+  end  
+  
+  # generate a JSON object that contains a message, username, and post_date
+  # intended to be passed as insert data to an elastic search server
+  # @return [JSON]
+  def generate_random_insert_data
+    {message: "#{random_tweet}", username: "#{random_username}", post_date: "#{current_timestamp}"}.to_json
+  end
+  
+end
---
+++ lib/rubberband_flamethrower/inserter.rb
@@ -0,0 +1,39 @@
+# Rubberband Flamethrower is a collection of scripts for dealing with faked Elastic Search data. 
+# This file is the main script for inserting fake data 
+# It inserts fake "tweet" type objects into a "twitter" index 
+# on a local Elastic Search server at localhost:9200.  
+# It runs in an infinite loop until you stop it.
+#
+#
+# Author::    Michael Orr 
+#             email - michael@cloudspace.com 
+#             twitter - @imbiat
+# Copyright:: Copyright (c) 2013
+# License::   MIT License
+
+
+require "httparty"
+require "active_support/core_ext"
+require_relative "models/data_generator.rb"
+
+# a unique ID must be provided for each document stored in Elastic Search
+# since we are looping, we set a starting id and it will be incremented in the loop
+id = 2
+
+# initialize the random data generator object
+data = DataGenerator.new
+
+# start infinite loop
+while true do  
+  
+  # generate a piece of random data to insert that approxiamates a tweet
+  insert_data = data.generate_random_insert_data
+  puts insert_data
+  
+  # insert the random data into local elastic search index "twitter" as type "tweet" with set id
+  response = HTTParty.put("http://localhost:9200/twitter/tweet/#{id}", body: insert_data) 
+  puts response.body
+  
+  #increment the insert id
+  id = id + 1
+end

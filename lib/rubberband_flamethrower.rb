cb47ce7c7862eb1882c7f6de132043bbd386b467
Michael Orr <michael@cloudspace.com>
2013-04-09 02:38:05 -0400

    updating help menu to include new auto command information

--- lib/rubberband_flamethrower.rb
+++ lib/rubberband_flamethrower.rb
@@ -35,8 +35,12 @@ module RubberbandFlamethrower
     end
   end
 
+  # Displays help menu of the available help menu commands
   def self.help
-    puts "Flamethrower Commands Available:\n\nflamethrower #display this help message\nflamethrower fire #sends a batch to the elastic search server"
+    puts "Flamethrower Commands Available:\n"
+    puts "flamethrower #display this help message"
+    puts "flamethrower fire #benchmark a batch insert of data to an elastic search server"
+    puts "flamethrower auto #benchmark a series of batch inserts to an elastic search server""
   end
 
 end

4afad82eab5bc9ce3e820e312cd5bd549db02fe6
Michael Orr <michael@cloudspace.com>
2013-04-09 04:33:11 +0000

    adding support for auto mode, which performs the fire command the number of times specified (or defaults to 3)

--- lib/rubberband_flamethrower.rb
+++ lib/rubberband_flamethrower.rb
@@ -7,10 +7,26 @@ module RubberbandFlamethrower
     require File.dirname(__FILE__)+"/rubberband_flamethrower/flamethrower.rb"
     flamethrower = Flamethrower.new
     time = Benchmark.measure do
-      flamethrower.fire(how_many, starting_id, server_url, index, type)
+      flamethrower.fire(how_many, starting_id, server_url, index, type, 1)
     end
+    puts "\nFinished Inserting #{how_many} documents into Elastic Search."
     puts "  user       system     total    real"
     puts time
+  end
+
+  def self.auto(how_many_batches=3, per_batch=5000, starting_id=1, server_url="http://localhost:9200", index="twitter", type="tweet")
+    require "active_support/core_ext"
+    require 'httparty'
+    require "benchmark"
+    require File.dirname(__FILE__)+"/rubberband_flamethrower/data_generator.rb"
+    require File.dirname(__FILE__)+"/rubberband_flamethrower/flamethrower.rb"
+    flamethrower = Flamethrower.new
+    Benchmark.bm(8) do |x|
+      how_many_batches.to_i.times do |i|
+        x.report("set #{i+1}:")   { flamethrower.fire(per_batch, starting_id, server_url, index, type) }
+        starting_id = starting_id.to_i + per_batch.to_i
+      end
+    end
   end
 
   def self.help
--- lib/rubberband_flamethrower/flamethrower.rb
+++ lib/rubberband_flamethrower/flamethrower.rb
@@ -1,9 +1,10 @@
 # This class is designed to insert objects created with the DataGenerator class into Elastic Search
 module RubberbandFlamethrower
   class Flamethrower
-    def fire(how_many, starting_id, server_url, index, type)
+    def fire(how_many, starting_id, server_url, index, type, printing_level=0)
 
-      $stdout.sync = true
+      
+      ($stdout.sync = true) if printing_level == 1
 
       # a unique ID must be provided for each document stored in Elastic Search
       id = starting_id.to_i
@@ -15,17 +16,16 @@ module RubberbandFlamethrower
 
         # generate a piece of random data to insert that approxiamates a tweet
         insert_data = data.generate_random_insert_data
-        #puts insert_data
+        (puts insert_data) if printing_level == 2
 
         # insert the random data into local elastic search index "twitter" as type "tweet" with set id
         response = HTTParty.put("#{server_url}/#{index}/#{type}/#{id}", body: insert_data) 
-        #puts response.body
-        print "."
+        (puts response.body) if printing_level == 2
+        (print ".") if printing_level == 1
 
         #increment the insert id
         id = id + 1
       end
-      puts "\n\nFinished Inserting #{how_many} documents into Elastic Search."
     end
     
   end

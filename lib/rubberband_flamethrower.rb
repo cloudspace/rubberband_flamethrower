1ce782b6be936a333d099acceff14a1388abed11
Michael Orr <michael@cloudspace.com>
2013-04-09 03:14:26 -0400

    updating help, changing default batch to 500

--- lib/rubberband_flamethrower.rb
+++ lib/rubberband_flamethrower.rb
@@ -6,7 +6,7 @@ module RubberbandFlamethrower
   # @param [String] server_url - url of the Elastic Search server
   # @param [String] index - name of the Elastic Search index to insert data into
   # @param [String] type - name of the Elastic Search type to insert data into
-  def self.fire(how_many=5000, starting_id=1, server_url="http://localhost:9200", index="twitter", type="tweet")
+  def self.fire(how_many=500, starting_id=1, server_url="http://localhost:9200", index="twitter", type="tweet")
     require File.dirname(__FILE__)+"/rubberband_flamethrower/flamethrower.rb"
     flamethrower = Flamethrower.new
     time = Benchmark.measure do
@@ -24,7 +24,7 @@ module RubberbandFlamethrower
   # @param [String] server_url - url of the Elastic Search server
   # @param [String] index - name of the Elastic Search index to insert data into
   # @param [String] type - name of the Elastic Search type to insert data into
-  def self.auto(how_many_batches=3, per_batch=5000, starting_id=1, server_url="http://localhost:9200", index="twitter", type="tweet")
+  def self.auto(how_many_batches=3, per_batch=500, starting_id=1, server_url="http://localhost:9200", index="twitter", type="tweet")
     require File.dirname(__FILE__)+"/rubberband_flamethrower/flamethrower.rb"
     flamethrower = Flamethrower.new
     Benchmark.bm(8) do |x|
@@ -39,26 +39,29 @@ module RubberbandFlamethrower
   def self.help
     puts "Rubberband Flamethrower is a gem for inserting faked data into an Elastic Search server"
     puts "and providing basic benchmarks. It creates and inserts fake data objects with three"
-    puts "fields (message, username, and post_date) and reports timing information.\n"
+    puts "fields (message, username, and post_date) and reports timing information."
     
-    puts "Flamethrower Commands Available:\n"
+    puts "\n\nFlamethrower Commands Available:\n\n"
     puts "flamethrower fire #benchmark a batch insert of data to an elastic search server"
     puts "flamethrower auto #benchmark a series of batch inserts to an elastic search server"
     puts "flamethrower help #display this help message"
     
-    puts "\nThe fire and auto commands can be configured by passing arguments."
-    puts "The parameters accepted by the `flamethrower fire` command all have a default value if"
-    puts "left blank. Here are the parameters in order with their default values:"
-    puts "(how_many=500, starting_id=1, server_url=http://localhost:9200, index=twitter, type=tweet)\n"
+    puts "\n\nThe fire and auto commands can be configured by passing arguments."
+    puts "The parameters accepted by fire and auto all have a default value if left blank."
     
-    puts "Command Examples Using Parameters:"
+    puts "\n\n\"fire\" parameters in order with their default values:"
+    puts "(how_many=500, starting_id=1, server_url=http://localhost:9200, index=twitter, type=tweet)"
+
+    puts "\n\n\"auto\" parameters in order with their default values:"
+    puts "(how_many_batches=3, per_batch=500, starting_id=1, server_url=http://localhost:9200, index=twitter, type=tweet)"
+    
+    puts "\n\nUsage Examples With Parameters:"
     puts "flamethrower fire 10000 #To Insert 10,000 instead of 500"
     puts "flamethrower fire 5000 5001 #To Insert 5,000 starting with the ID 5001"
-    puts "flamethrower fire 2000 1 \"http://es.com:9200\" #To Insert 2,000 starting with the ID 1 to"
-    puts "\t\t\ta server located at http://es.com:9200"
-    puts "flamethrower fire 500 1 \"http://localhost:9200\" \"facebook\" \"message\" #To put your"
-    puts "\t\t\tdocuments into an index named \"facebook\" instead of \"twitter\" with a type of"
-    puts "\t\t\t\"message\" instead of \"tweet\""
+    puts "flamethrower fire 2000 1 \"http://es.com:9200\" #Elastic Search Node located at http://es.com:9200"
+    puts "flamethrower fire 500 1 \"http://localhost:9200\" \"facebook\" \"message\"
+    puts "\t#Insert into an index named \"facebook\" instead of \"twitter\""
+    puts "\t#with a type of \"message\" instead of \"tweet\""
   end
 
 end
